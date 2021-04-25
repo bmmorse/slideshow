a665935479cba6b609c7bbf5e7aab3dcb2a74f55
Brendan Morse <42715623+bmmorse@users.noreply.github.com>
2021-04-25 03:05:27 -0400

    ps update

--- client/src/components/PS/Banner.js
+++ client/src/components/PS/Banner.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const DIV_WRAPPER = styled.div`
   align-items: center;
   background: url('https://www.psitmatters.com/wp-content/uploads/2020/11/tucker-tangeman-SnXraH8PaQ4-unsplash-scaled.jpg')
     no-repeat;
   background-position: center center;
-  background-size: auto 100%;
+  background-size: cover;
+
   display: flex;
-  height: 600px;
+  height: 560px;
   justify-content: center;
   position: relative;
   width: 100%;
+
+  @media (min-width: 1000px) {
+    height: 640px;
+  }
 
   &::after {
     background: linear-gradient(0deg, #00000092 30%, #00000064 100%);
@@ -21,10 +26,6 @@ const DIV_WRAPPER = styled.div`
     position: absolute;
     left: 0;
     top: 0;
-  }
-
-  @media (min-width: 900px) {
-    background-size: 100% auto;
   }
 `;
 
@@ -36,17 +37,24 @@ const H1_BANNER = styled.h1`
   letter-spacing: 0;
   text-align: center;
   position: relative;
-  bottom: 50px;
+  bottom: 100px;
   z-index: 10;
   width: 300px;
 
   @media (min-width: 500px) {
+    font-size: 40px;
     width: 500px;
+  }
+
+  @media (min-width: 800px) {
+    width: 500px;
+    bottom: 50px;
     font-size: 40px;
   }
 
   @media (min-width: 1000px) {
     width: 770px;
+    bottom: 0px;
     font-size: 64px;
   }
 `;
@@ -68,9 +76,25 @@ const DIV_TEXT = styled.div`
   }
 `;
 
+const animateItem = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(1);
+  }
+
+  75% {
+    transform: scale(1.2);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
+
 const UL_NUMBERS = styled.ul`
   display: grid;
-  gap: 20px 10px;
+  gap: 40px 10px;
   grid-template-columns: 1fr 1fr;
   height: 100%;
   width: 100%;
@@ -86,6 +110,11 @@ const UL_NUMBERS = styled.ul`
   li {
     text-align: center;
     flex: 1 1 30%;
+    opacity: 0;
+
+    &.animate {
+      animation: ${animateItem} 500ms ease forwards;
+    }
 
     &:hover {
       h3 {
@@ -117,6 +146,26 @@ const UL_NUMBERS = styled.ul`
 `;
 
 export default function Banner() {
+  const animateNumbersRef = React.useRef(null);
+
+  const [animateNumbers, setAnimateNumbers] = React.useState(false);
+
+  React.useEffect(() => {
+    function createTimer(child, delay) {
+      return setTimeout(() => {
+        list.children[child].classList.add('animate');
+      }, delay);
+    }
+
+    const list = animateNumbersRef.current;
+    createTimer(0, 1000);
+    createTimer(1, 1500);
+    createTimer(2, 2000);
+    createTimer(3, 2500);
+    createTimer(4, 3000);
+    createTimer(5, 3500);
+  });
+
   const figures = [
     ['$4,769,933', 'Program Donations'],
     ['31,851', 'Non-Profits Supported'],
@@ -129,10 +178,10 @@ export default function Banner() {
     <DIV_WRAPPER>
       <H1_BANNER>Everyday choices can change the world.</H1_BANNER>
       <DIV_TEXT>
-        <UL_NUMBERS>
+        <UL_NUMBERS ref={animateNumbersRef}>
           {figures.map((e) => {
             return (
-              <li>
+              <li key={e[1]}>
                 <h3>{e[0]}</h3>
                 <p>{e[1]}</p>
               </li>
